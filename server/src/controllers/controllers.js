cd0bd7f2e6972ee6e7d850a3bdcafc437ef7dfe6
Tatiana Medina <tatymedina284@gmail.com>
2024-08-05 19:54:41 -0300

    se terminó de hacer el crud

--- server/src/controllers/controllers.js
+++ server/src/controllers/controllers.js
@@ -9,6 +9,8 @@ ctrl.getTasks = async (req, res)=>{
     const [ results ] = await connection.query('SELECT * FROM tasks');
 
     return res.json(results);
+
+    connection.end
 }
 ctrl.addTasks = async (req, res)=>{
 
@@ -18,23 +20,48 @@ ctrl.addTasks = async (req, res)=>{
 
     const conexion = await connection.query(`INSERT INTO tasks (title, description, isComplete) VALUES ("${title}","${description}",${isComplete})`)
 
-    if(conexion ){
-        console.log('se añadio con exito')
-    }
+    res.send("tarea agregada")
+
+   connection.end
 
 }
 ctrl.getById = async(req,res)=>{
 
-    const connection = await connectDB();
-
     const id = req.params.id 
 
-    const conexion = await connection.query(`SELECT * FROM tasks WHERE id=${id}`)
+    const connection = await connectDB();
+
+
+    const results = await connection.query(`SELECT * FROM tasks WHERE id=?`, id)
     
-    if(conexion){
-        res.json(conexion)
-    }
+   res.json(results[0])
+
+   connection.end
 
 }
-ctrl.delete = async(req,res)=>
+ctrl.deleteTasks = async(req,res)=>{
+    const id = parseInt(req.params.id )
+
+    const connection = await connectDB();
+
+    const results = await connection.query(`DELETE FROM tasks WHERE id = ?`, id)
+
+    res.send("tarea eliminada")
+
+}
+
+ctrl.editTasks = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, description, isComplete } = req.body;
+    const connection = await connectDB();
+
+    
+    const results = await connection.query(
+        `UPDATE tasks SET title = ?, description = ?, isComplete = ? WHERE id = ?`,
+        [title, description, isComplete, id] 
+    );
+
+    res.json(results[0]);
+};
+
 module.exports = ctrl;
--- server/src/routes/app.routes.js
+++ server/src/routes/app.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { getTasks, addTasks, getById } = require('../controllers/controllers')
+const { getTasks, addTasks, getById, deleteTasks, editTasks } = require('../controllers/controllers')
 const router = Router();
 
 router.get('/tasks', getTasks);
 router.post('/tasks',addTasks);
-router.get('/tasks/:id', getById)
+router.get('/tasks/:id', getById);
+router.delete('/tasks/:id',deleteTasks);
+router.put('/tasks/:id', editTasks);
 
 module.exports = router;
