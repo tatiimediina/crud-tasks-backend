4c25dd38ddb0ca57c6ad8d9fea58afeb9cc7fe3e
Tatiana Medina <tatymedina284@gmail.com>
2024-08-16 11:14:37 -0300

    se arreglo el controlador de añadir

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,19 @@
-import express, { json } from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-
+import express, { json } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
-app.use(json())
+app.use(json());
 
 const port = process.env.port || 3000;
 
-import { router } from "./routes/app.routes.js"
+import { router } from "./routes/app.routes.js";
 
-app.use('/tasks', router)
+app.use("/tasks", router);
 
-app.listen(port,()=>{
-    console.log(`server on port ${port}`)
-}
-)
+app.listen(port, () => {
+  console.log(`server on port ${port}`);
+});
--- server/src/controllers/controllers.js
+++ server/src/controllers/controllers.js
@@ -1,126 +1,133 @@
 import { connectDB } from "../db/database.js";
 
 export const validateTaskData = (title, description, isComplete) => {
-    if (typeof title !== 'string' || title.trim() === '' || title.length > 255) {
-        return { valid: false, message: 'El titulo debe ser una cadena no vacía de un máximo de 255 caracteres' };
-    }
-    if (typeof description !== 'string' || description.trim() === '') {
-        return { valid: false, message: 'La descripción debe ser una cadena no vacía.' };
-    }
-    if (typeof isComplete !== 'boolean') {
-        return { valid: false, message: 'isComplete debe ser un valor booleano.' };
-    }
-    return { valid: true };
-}
+  if (typeof title !== "string" || title.trim() === "" || title.length > 255) {
+    return {
+      valid: false,
+      message:
+        "El titulo debe ser una cadena no vacía de un máximo de 255 caracteres",
+    };
+  }
+  if (typeof description !== "string" || description.trim() === "") {
+    return {
+      valid: false,
+      message: "La descripción debe ser una cadena no vacía.",
+    };
+  }
+  if (typeof isComplete !== "boolean") {
+    return { valid: false, message: "isComplete debe ser un valor booleano." };
+  }
+  return { valid: true };
+};
 
 export const getTasks = async (req, res) => {
-    try {
-        const connection = await connectDB();
-        const [results] = await connection.query('SELECT * FROM tasks');
-        return res.status(200).json(results);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Error al obtener tareas' });
-    } 
+  try {
+    const connection = await connectDB();
+    const [results] = await connection.query("SELECT * FROM tasks");
+    return res.status(200).json(results);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error al obtener tareas" });
+  }
 };
 
 export const addTasks = async (req, res) => {
-    const { title, description, isComplete } = req.body;
+  const { title, description } = req.body;
 
-    const validation = validateTaskData(title, description, isComplete);
-    if (!validation.valid) {
-        return res.status(400).json({ error: validation.message });
-    }
+  try {
+    const connection = await connectDB();
+    await connection.query(
+      "INSERT INTO tasks (title, description VALUES (?, ?)",
+      [title, description]
+    );
 
-    try {
-        const connection = await connectDB();
-        await connection.query('INSERT INTO tasks (title, description, isComplete) VALUES (?, ?, ?)', [title, description, isComplete]);
-
-        return res.status(201).json({ message: 'Tarea añadida' });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Error al añadir tarea' });
-
-    } 
+    return res.status(201).json({ message: "Tarea añadida" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error al añadir tarea" });
+  }
 };
 export const getById = async (req, res) => {
-    const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-    if (isNaN(id)) {
-        console.log('')
-        return res.status(400).json({ error: 'id no válido' }) 
-        
+  if (isNaN(id)) {
+    console.log("");
+    return res.status(400).json({ error: "id no válido" });
+  }
+
+  try {
+    const connection = await connectDB();
+    const [results] = await connection.query(
+      "SELECT * FROM tasks WHERE id = ?",
+      [id]
+    );
+    connection.end();
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: "Tarea no encontrada" });
     }
 
-    try {
-        const connection = await connectDB();
-        const [results] = await connection.query('SELECT * FROM tasks WHERE id = ?', [id]);
-        connection.end();
-
-        if (results.length === 0) {
-            return res.status(404).json({ error: 'Tarea no encontrada' });
-        }
-
-        return res.status(200).json(results[0]);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Error al obtener la tarea' });
-    }
+    return res.status(200).json(results[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error al obtener la tarea" });
+  }
 };
 
 export const deleteTasks = async (req, res) => {
-    const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-    if (isNaN(id)) {
-        return res.status(400).json({ error: 'id no válido' });
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "id no válido" });
+  }
+
+  try {
+    const connection = await connectDB();
+    const [results] = await connection.query("DELETE FROM tasks WHERE id = ?", [
+      id,
+    ]);
+
+    if (res.status(404)) {
+      res.send({ message: "La tarea no existe" });
+    } else {
+      res.status(200).json({ message: "Tarea eliminada" });
     }
 
-    try {
-        const connection = await connectDB();
-        const [results] = await connection.query('DELETE FROM tasks WHERE id = ?', [id]);
-
-        if(res.status(404)){
-            res.send({message: "La tarea no existe"})
-        } else {
-        res.status(200).json({ message: 'Tarea eliminada' });
-        }
-
-        connection.end()
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Error al eliminar tarea' });
-    }
+    connection.end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error al eliminar tarea" });
+  }
 };
 
 export const editTasks = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const { title, description, isComplete } = req.body;
+  const id = parseInt(req.params.id);
+  const { title, description, isComplete } = req.body;
 
-    if (isNaN(id)) {
-        return res.status(400).json({ error: 'id no válido' });
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "id no válido" });
+  }
+
+  const validation = validateTaskData(title, description, isComplete);
+  if (!validation.valid) {
+    return res.status(400).json({ error: validation.message });
+  }
+
+  try {
+    const connection = await connectDB();
+    const [results] = await connection.query(
+      "UPDATE tasks SET title = ?, description = ?, isComplete = ? WHERE id = ?",
+      [title, description, isComplete, id]
+    );
+
+    if (res.status(404)) {
+      res.send({ message: "La tarea no existe" });
+    } else {
+      res.status(200).json({ message: "Tarea eliminada" });
     }
-
-    const validation = validateTaskData(title, description, isComplete);
-    if (!validation.valid) {
-        return res.status(400).json({ error: validation.message });
-    }
-
-    try {
-        const connection = await connectDB();
-        const [results] = await connection.query('UPDATE tasks SET title = ?, description = ?, isComplete = ? WHERE id = ?', [title, description, isComplete, id]);
-
-        if(res.status(404)){
-            res.send({message: "La tarea no existe"})
-        } else {
-        res.status(200).json({ message: 'Tarea eliminada' });
-        }
-        connection.end();
-
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Error al editar tarea' });
-    }
+    connection.end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error al editar tarea" });
+  }
 };
-
-
